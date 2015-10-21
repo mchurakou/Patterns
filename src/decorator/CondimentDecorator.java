3facf40b2517d11eb9c39c1ad620cf7713249a2e
mchurakou <1badbug1@gmail.com>
2015-10-21 23:33:40 +0300

    - decorator rafactoring

--- src/decorator/CondimentDecorator.java
+++ src/decorator/CondimentDecorator.java
@@ -1,7 +1,11 @@
 package decorator;
 
 public abstract class CondimentDecorator extends Beverage {
+	public CondimentDecorator(Beverage b) {
+		this.b = b;
+	}
 
+	protected Beverage b;
 	
 	public abstract String getDescription();
 
